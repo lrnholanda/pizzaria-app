940565824a9cf0045bc15850a43efbd09376f580
lrnholanda <lrnholanda@gmail.com>
2023-05-22 17:09:41 -0300

    added new featured

--- Pages/PizzaList.cshtml.cs
+++ Pages/PizzaList.cshtml.cs
@@ -9,6 +9,8 @@ namespace ContosoPizza.Pages
    public class PizzaListModel : PageModel
    {
     private readonly PizzaService _service;
+    [BindProperty]
+    public Pizza NewPizza { get; set; } = default!;
     public IList<Pizza> PizzaList { get;set; } = default!;
 
     public PizzaListModel(PizzaService service)
@@ -19,6 +21,23 @@ namespace ContosoPizza.Pages
     public void OnGet()
     {
         PizzaList = _service.GetPizzas();
+    }
+
+    public IActionResult OnPost()
+    {
+        if(!ModelState.IsValid || NewPizza == null)
+        {
+            return Page();
+        }
+
+        _service.AddPizza(NewPizza);
+        return RedirectToAction("Get");
+    }
+
+    public IActionResult OnPostDelete(int id)
+    {
+        _service.DeletePizza(id);
+        return RedirectToAction("Get");
     }
   }
 }
